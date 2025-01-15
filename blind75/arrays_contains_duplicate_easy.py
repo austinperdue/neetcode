96011c01046a32689a7e824cdf9e5ccb0ff1670a
austinperdue <47020337+austinperdue@users.noreply.github.com>
2025-01-15 15:10:15 -0500

    Added Blind75
    Arrays contains duplicate easy question added

---
+++ blind75/arrays_contains_duplicate_easy.py
@@ -0,0 +1,28 @@
+# Contains Duplicate
+# Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+# Example 1:
+
+# Input: nums = [1, 2, 3, 3]
+
+# Output: true
+
+# Example 2:
+
+# Input: nums = [1, 2, 3, 4]
+
+# Output: false
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = {}
+
+        for num in nums:
+            if num in seen:
+                return True # duplicate found
+            seen[num] = True # add num to seen dict.
+        return False # no duplicates found
+
+
+        
+         
